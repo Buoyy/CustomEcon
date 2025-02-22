0ab75e440fdacd8c30a929899abd7d692c24d672
Buoyy <81320656+Buoyy@users.noreply.github.com>
2025-02-22 18:47:14 +0500

    Update InvButton code (will need later)

--- src/main/java/com/github/buoyy/buoyyecon/gui/InvButton.java
+++ src/main/java/com/github/buoyy/buoyyecon/gui/InvButton.java
@@ -12,45 +12,48 @@ import java.util.function.Consumer;
 public class InvButton {
     private final ItemStack icon;
     private final Consumer<InventoryClickEvent> onClick;
+
     private InvButton(Builder builder) {
         this.icon = builder.icon;
         this.onClick = builder.onClick;
     }
+
     public ItemStack getIcon() {
         return icon;
     }
+
     public Consumer<InventoryClickEvent> getOnClick() {
         return onClick;
     }
+
     public static class Builder {
         private ItemStack icon;
-        private Material display;
-        private String name;
-        private String[] lore;
         private Consumer<InventoryClickEvent> onClick;
+
         public static Builder newBuilder() {
             return new Builder();
         }
-        public Builder setDisplay(Material display) {
-            this.display = display;
+        public Builder setIcon(Material icon) {
+            this.icon = new ItemStack(icon);
             return this;
         }
+
         public Builder setName(String name) {
-            this.name = name;
+            
+            this.icon.getItemMeta().setDisplayName(name);
             return this;
         }
+
         public Builder setLore(String... lore) {
-            this.lore = lore;
+            this.icon.getItemMeta().setLore(Arrays.asList(lore));      
+            return this;
+        }
+        public Builder setOnClick(Consumer<InventoryClickEvent> onClick) {
+            this.onClick = onClick;
             return this;
         }
         public InvButton build() {
-            this.icon = new ItemStack(this.display);
-            ItemMeta meta = this.icon.getItemMeta();
-            meta.setDisplayName(this.name);
-            meta.setLore(Arrays.asList(this.lore));
-            this.icon.setItemMeta(meta);
             return new InvButton(this);
         }
     }
 }
-
