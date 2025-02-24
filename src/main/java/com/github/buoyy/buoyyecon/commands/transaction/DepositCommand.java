95b34457a3783dd020977696626aefc2ca0f20f1
Buoyy <saarthak0902@gmail.com>
2025-02-24 18:22:40 +0500

    Fix gold deposit error

--- src/main/java/com/github/buoyy/buoyyecon/commands/transaction/DepositCommand.java
+++ src/main/java/com/github/buoyy/buoyyecon/commands/transaction/DepositCommand.java
@@ -6,7 +6,6 @@ import com.github.buoyy.api.economy.Economy;
 import com.github.buoyy.api.economy.Transaction;
 import com.github.buoyy.buoyyecon.BuoyyEcon;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -16,14 +15,16 @@ import java.util.List;
 public class DepositCommand implements SubCommand {
     private final Economy econ;
     private final CurrencyType type;
+
     public DepositCommand(CurrencyType type) {
         this.econ = BuoyyEcon.getEconomy();
         this.type = type;
     }
+
     @Override
     public boolean execute(CommandSender sender, String[] args) {
         if (!(sender instanceof Player player)) {
-            sender.sendMessage(ChatColor.RED+"Can't add to console!");
+            sender.sendMessage(ChatColor.RED + "Can't add to console!");
             return true;
         }
         if (args.length < 2) {
@@ -33,7 +34,7 @@ public class DepositCommand implements SubCommand {
         int oldBalance = econ.getBalance(player, type);
         int amount = 0;
         if (args[1].equals("all")) {
-            for (ItemStack i: player.getInventory().getStorageContents()) {
+            for (ItemStack i : player.getInventory().getStorageContents()) {
                 if (i != null && i.getType() == type.getMaterial()) {
                     amount += i.getAmount();
                 }
@@ -41,23 +42,23 @@ public class DepositCommand implements SubCommand {
         } else try {
             amount = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            player.sendMessage(ChatColor.RED+"That's not a number.");
+            player.sendMessage(ChatColor.RED + "That's not a number.");
             return true;
         }
         if (amount == 0) {
-            player.sendMessage(ChatColor.RED + "Can't add zero "+type.getNamePlural());
+            player.sendMessage(ChatColor.RED + "Can't add zero " + type.getNamePlural());
             return true;
         }
-        if (!(player.getInventory().contains(Material.DIAMOND, amount))) {
+        if (!(player.getInventory().contains(type.getMaterial(), amount))) {
             player.sendMessage(ChatColor.RED + "You don't have " + econ.format(amount, type) +
                     " in your inventory!");
             return true;
         }
         if (getAvailableSpace(player) == 0) {
-            player.sendMessage(ChatColor.RED+"You don't have more storage space!");
+            player.sendMessage(ChatColor.RED + "You don't have more storage space!");
             return true;
         }
-        player.getInventory().removeItem(new ItemStack(Material.DIAMOND, amount));
+        player.getInventory().removeItem(new ItemStack(type.getMaterial(), amount));
         Transaction deposition = econ.add(player, type, amount);
         if (!deposition.isSuccessful()) {
             sender.sendMessage(ChatColor.DARK_RED + "Error: " + ChatColor.RED + deposition.message);
@@ -68,11 +69,12 @@ public class DepositCommand implements SubCommand {
             player.getWorld()
                     .dropItemNaturally(player.getLocation(),
                             new ItemStack(type.getMaterial(), notDeposited));
-            player.sendMessage(ChatColor.DARK_GREEN+econ.format(notDeposited, type)+
+            player.sendMessage(ChatColor.DARK_GREEN + econ.format(notDeposited, type) +
                     " have been dropped to you as they couldn't be stored.");
         }
         return true;
     }
+
     @Override
     public List<String> getCompletions(String[] args) {
         return args.length == 2 ?
